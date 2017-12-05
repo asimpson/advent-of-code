ed2bbcc3afa4776d1ac55e383cefc1976290226c
Adam Simpson <adam@heysparkbox.com>
2017-12-05 09:56:56 -0500

    feat: day 4 success in lisp
    - some optimizations here and there, see [reddit thread][thread] for
      some of them.
    
    [thread]: https://www.reddit.com/r/lisp/comments/7hmcip/advent_of_code_day_4_solution_in_lisp/

---
+++ day-4/day-4.lisp
@@ -0,0 +1,48 @@
+;; To ensure security, a valid passphrase must contain no duplicate words.
+
+;; For example:
+
+;; aa bb cc dd ee is valid.
+;; aa bb cc dd aa is not valid - the word aa appears more than once.
+;; aa bb cc dd aaa is valid - aa and aaa count as different words.
+;; The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+;; --- Part Two ---
+
+;; For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+;; For example:
+
+;; abcde fghij is a valid passphrase.
+;; abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+;; a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+;; iiii oiii ooii oooi oooo is valid.
+;; oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+(ql:quickload :cl-strings)
+
+(defun process(line)
+  (when (eql (length line) (length (remove-duplicates line :test #'equal)))
+    (return-from process 1)))
+
+(defun ana(line)
+  (let (m)
+    (setq m (map 'sequence (lambda(x) (sort x #'char>)) line))
+    (when (eql (length line) (length (remove-duplicates m :test #'equal)))
+      (return-from ana 1))))
+
+(let (first-count second-count first second)
+  (setq first-count 0)
+  (setq second-count 0)
+  (with-open-file (file "./input.txt")
+                  (loop for line = (read-line file nil 'end)
+                        until (eq line 'end)
+                        do (progn
+                             (setq first (process (cl-strings:split line " ")))
+                             (setq second (ana (cl-strings:split line " ")))
+                             (when (numberp first)
+                               (setq first-count (+ first-count first)))
+                             (when (numberp second)
+                               (setq second-count (+ second-count second))))
+                        finally (print (concatenate 'string "
+First rule: there are " (write-to-string first-count) " valid passphrases" "
+Second rule: there are " (write-to-string second-count) " valid passphrases.")))))
