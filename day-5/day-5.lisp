87dc866fb166f7628e56e6a39c1a7ed2caf05cf7
Adam Simpson <adam@heysparkbox.com>
2017-12-07 10:28:39 -0500

    refactor: day-5 lisp is now ~40ms

--- day-5/day-5.lisp
+++ day-5/day-5.lisp
@@ -1,16 +1,10 @@
-;; The message includes a list of the offsets for each jump. Jumps are relative: -1 moves to the previous instruction, and 2 skips the next one. Start at the first instruction in the list. The goal is to follow the jumps until one leads outside the list.
-
-;; In addition, these instructions are a little strange; after each jump, the offset of that instruction increases by 1. So, if you come across an offset of 3, you would move three instructions forward, but change it to a 4 for the next time it is encountered.
-
-;; How many steps does it take to reach the exit?
-(defun process(cells moves position)
-  (let ((spot (nth position cells)))
-    (if spot
-        (progn
-          (setq position (+ position (parse-integer spot)))
-          (nsubstitute (write-to-string (+ (parse-integer spot) 1)) spot cells)
-          (process cells (+ moves 1) position))
-        (print moves))))
+(defun process(cells)
+  (do ((moves 0 (+ 1 moves))
+       (position 0))
+      ((null (ignore-errors (elt cells position))) (print moves))
+    (let* ((spot (elt cells position)) (pos (+ spot position)))
+      (setf (elt cells position) (+ spot 1))
+      (setq position pos))))
 
 (defun check-offset(spot)
   (if (>= spot 3)
@@ -30,9 +24,10 @@
 (second-process '("0" "3" "0" "1" "-3") 0 0)
 
 (defun start()
-  (let (container)
-    (with-open-file (file "./input.txt")
-      (loop for line = (read-line file nil 'end)
-         until (eq line 'end)
-         do (push line container)
-         finally (process (reverse container) 0 0)))))
+  (with-open-file (file "./input.txt")
+    (loop for line = (read-line file nil 'end)
+       until (eq line 'end)
+       collect line into container
+       finally (process (map 'vector #'parse-integer container)))))
+
+(time (start))
